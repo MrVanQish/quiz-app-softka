160a8352fa9c2deee18bed74a919385c835dd6ed
Jacobo Hernandez <jacoboecheve99@gmail.com>
2021-09-25 09:56:21 -0500

    juego finalizado

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -13,7 +13,6 @@ import { DbService } from 'src/app/services/db.service';
   styleUrls: ['./quiz.component.css'],
 })
 export class QuizComponent implements OnInit {
-
   public questionsArray: Array<Questions> = [];
   public acc = 1;
   public questionsNumMax = 0;
@@ -21,7 +20,6 @@ export class QuizComponent implements OnInit {
   public arrayAux: Array<String> = [];
   public scores: Array<Score> = [];
   public loading: boolean = false;
-  public screenNickname : boolean = false;
 
   constructor(
     private questions: QuestionsService,
@@ -54,7 +52,6 @@ export class QuizComponent implements OnInit {
 
   public onCorrectAnswer(e: any, answer: any) {
     if (answer === this.questionsArray[0].correct_answer) {
-      
       setTimeout(() => {
         e.target.checked = false;
 
@@ -67,62 +64,69 @@ export class QuizComponent implements OnInit {
 
           this.points += 10;
 
-          this.acc === this.questionsNumMax + 1
+  /*           this.acc === this.questionsNumMax + 1
             ? this.router.navigate(['/home'])
-            : console.log(this.questionsArray);
+            : console.log(this.questionsArray); */
 
-           
-            console.log(this.scores);
 
         } else {
 
-          this.screenNickname = true;
-
           this.points += 10;
-          this.acc === this.questionsNumMax + 1;
+/*           this.acc === this.questionsNumMax + 1; */
 
-          this.saveDataWinnerOrLoser('winner')
-          
-          setTimeout(() => {
+          alert('WOW! You won')
 
+          const nickname = prompt("Please enter your nickname", "");
+
+          this.saveDataWinnerOrLoser(String(nickname));
+
+          this.router.navigate(['/home'])
+
+    /*       setTimeout(() => {
             this.router.navigate(['/home']);
-
-          }, 7000);
-
-          
+          }, 7000); */
         }
       }, 700);
     } else {
 
-      this.screenNickname = true;
-
-      this.saveDataWinnerOrLoser('loser')
-
       setTimeout(() => {
+
+        alert('WRONGGGGG! Has perdido')
+
+        const nickname = prompt("Please enter your nickname", "");
+
+        this.saveDataWinnerOrLoser(String(nickname));
+
         this.router.navigate(['/home']);
-      }, 7000);
+
+      }, 500);
+
+      
+
+      
     }
   }
 
-  public saveDataWinnerOrLoser(name: string){
+  public leaveAndSaveProgress(){
 
-    if(this.points < 50){
+    const nickname = prompt("Please enter your nickname", "");
 
+    const playerInfo = new Score(this.points, String(nickname), this.points / 10);
+
+    this.dbService.addScore(playerInfo).subscribe();
+  }
+
+  public saveDataWinnerOrLoser(name: string) {
+    if (this.points <= 40) {
       const playerInfo = new Score(0, name, 0);
 
       this.dbService.addScore(playerInfo).subscribe();
-
-    }else{
-
+    } else {
       const playerInfo = new Score(this.points, name, this.points / 10);
 
       this.dbService.addScore(playerInfo).subscribe();
-
     }
-   
-
   }
-
 
   public answersRandom() {
     const answerErrors = this.questionsArray[0].incorrect_answers;
@@ -135,11 +139,12 @@ export class QuizComponent implements OnInit {
     console.log(answerCorrect);
     console.log(this.questionsArray);
 
-  /*   console.log(this.arrayAux); */
+    /*   console.log(this.arrayAux); */
   }
 
   public shuffle(array: any) {
-    let currentIndex = array.length, randomIndex;
+    let currentIndex = array.length,
+      randomIndex;
 
     // While there remain elements to shuffle...
     while (currentIndex != 0) {
